392378551d7eb2938169cc27af13af3aea96d568
tatsu <tatsuhiro_takahashi_aa@mail.toyota.co.jp>
2024-06-10 12:48:56 +0900

    WIP 問６

--- A. VariousAPIList/0_Template/VariousAPIList/VariousAPIListTests/TestDoubles/SpyURLSettion.swift
+++ A. VariousAPIList/0_Template/VariousAPIList/VariousAPIListTests/TestDoubles/SpyURLSettion.swift
@@ -2,12 +2,28 @@ import Foundation
 
 @testable import VariousAPIList
 
-//class SpyURLSettion: MyURLSession {
-//    func dataTask(with request: URLRequest, completionHandler: @escaping @Sendable (Data?, URLResponse?, (any Error)?) -> Void) -> URLSessionDataTask {
-//        
-//    }
-//    
-//    func data(from url: URL) async throws -> (Data, URLResponse) {
-//        
-//    }
-//}
+class SpyURLSettion: MyURLSession {
+    var spyTask: SpyURLSessionDataTask?
+    var dataTask_arg: URLRequest?
+    
+    func dataTask(with request: URLRequest, completionHandler: @escaping @Sendable (Data?, URLResponse?, (any Error)?) -> Void) -> URLSessionDataTask {
+        
+        let task = URLSessionTask()
+        return URLSessionDataTask()
+    }
+    
+    func data(from url: URL) async throws -> (Data, URLResponse) {
+        return (Data(), URLResponse())
+    }
+}
+
+class SpyURLSessionDataTask: URLSessionTask {
+    var data: Data?
+    var _response: URLResponse?
+    var _error: Error?
+    var completionHandler:  (@Sendable (Data?, URLResponse?, (any Error)?) -> Void)?
+    
+    override func resume() {
+        completionHandler?(data, _response, _error)
+    }
+}
